7037805ab584df78cdad02719d1505222cf52117
Harshal Dhir <unixguru2k@Harshals-MacBook-Pro.local>
2023-11-04 16:37:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,149 @@
+## A Machaao Chatbot Template using Mistral-Instruct as LLM ##
+The intent of this template is to serve as a quick intro guide for fellow developers 
+looking to build AI Powered Personalized Characters using the latest [Mistral](https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.1) Large Language Model
+
+## Requirements for running it locally on laptop ##
+* Windows / Mac / Linux with Git installed
+* Python 3.8
+* MessengerX.io API Token - FREE for Indie Developers
+* Ngrok for Tunneling
+* Desktop / Laptop with a minimum of 16GB RAM
+* Huggingface hub API token - Follow [these](https://huggingface.co/docs/hub/security-tokens) instructions to generate one
+* Machaao API Token - Follow the instructions below to get your token
+
+## Get MessengerX.io API Key ##
+* Available on the [MessengerX.io](https://portal.messengerx.io/index#!/dashboard) portal
+* If you aren't registered, please create an account and login
+* Set up your new bot by providing it a `Character Name` and `Description`. 
+  * Select `Custom Bot` option
+  * It should look something like this:
+  * ![figure](https://github.com/machaao/machaao-py/raw/master/images/bot_setup.png?raw=true)
+* Click on `Save`. It will redirect you to your dashboard.
+* On your dashboard you can see your newly created bot
+  * ![figure](https://github.com/machaao/machaao-py/raw/master/images/new_bot.png?raw=true)
+* Click on `Settings` tab. It will open your bot configuration page.
+  * ![figure](https://github.com/machaao/machaao-py/raw/master/images/bot_config.png?raw=true)
+* On the configuration page you'd be able to see a string named `token`. That's your `Machaao API Token`
+
+## Local Setup ##
+### Download or clone this repository ###
+```
+git clone https://github.com/machaao/mistral-7b-chatbot.git
+
+cd mistral-7b-chatbot
+```
+
+### Install requirements ###
+```bash
+pip install -r requirements.txt
+```
+
+### Create a new .env file in the gpt-j-chatbot directory ###
+```bash
+nano -w .env
+```
+Put these key-value pairs in your .env file
+```
+API_TOKEN=<Machaao API Token>
+BASE_URL=https://ganglia.machaao.com
+NAME=Jess
+HUGGINGFACEHUB_API_TOKEN=<YOUR_HUGGINGFACEHUB API TOKEN> 
+# MODEL PARAMS - Unset Parameters would use their default values.
+MODEL_NAME=mistralai/Mistral-7B-Instruct-v0.1 # Mistral-Instruct model from Huggingface
+# Don't use Top_p and Temperature parameters simultaneously.
+# The higher this value, the less deterministic the result will be [`top_p` must be > 0.0 and < 1.0]
+TOP_P="0.9" 
+# The higher this value, the less deterministic the result will be
+TEMPERATURE="0.8"
+# The lower this value, the less likely GPT-J is going to generate off-topic text
+TOP_K="50"
+# The maximum number of tokens that the generated text should contain
+MAX_LENGTH="512"
+```
+
+### Modify logic/prompt.txt to change the character script ###
+```
+This is a conversation between [name] and user.
+Always generate grammatically correct sentences.
+[name] is a very understanding girl.
+[name] and user are seeing each other.
+Act as [name] and respond to the recent discussion between user and [name]
+```
+
+### Modify the core() function in logic/bot_logic.py to personalize responses ###
+```
+def core(self, req: str, user_id: str):
+```
+* Refer to [platform documentation](https://messengerx.rtfd.iio) for personalization options
+
+### Run the chatbot server from the root directory of the repo ###
+```
+python app.py
+```
+
+### Start ngrok.io tunnel ###
+```
+ngrok http 5000
+```
+* You'll get a `Forwarding` URL mentioned on the console as shown below
+  * ![figure](https://github.com/machaao/machaao-py/raw/master/images/ngrok_console.png?raw=true)
+* Copy the `Forwarding` URL. In this example it would be:
+```
+https://26ea-150-107-177-46.ngrok-free.app
+```
+
+### Update your webhook ###
+Update your bot `Webhook URL` on the bot configuration page with the NGROK `Forwarding URL`<br/>
+In this example your Webhook URL would be:
+```
+https://26ea-150-107-177-46.ngrok-free.app/machaao/hook
+```
+Refer to this screenshot below
+![figure](https://github.com/machaao/machaao-py/raw/master/images/update_hook.png?raw=true)
+
+### Test your bot:
+Click on `Preview` to chat with your bot
+
+  
+### Get Dashbot.io API KEY (Recommended for Production) ###
+* You can acquire the API Key via [Dashbot.io](https://dashbot.io) and replace it in the ```.env``` file under the entry
+```DASHBOT_KEY```
+
+## Remote Setup (Heroku) ##
+
+We are assuming you have access to a [heroku account](https://heroku.com)
+and have installed heroku command line client for your OS.
+
+### Login to Heroku ###
+```
+heroku login
+```
+
+### Create a new app on Heroku and note down your heroku app name
+```
+heroku create
+```
+
+### Commit changes and push the repository to Heroku ###
+```
+git commit -m ".env updated"
+git push heroku master
+```
+
+### Open the logs to confirm successful Deployment ###
+```
+heroku logs --tail
+```
+
+### Update your webhook ###
+Update your bot Webhook URL at [MessengerX.io Portal](https://portal.messengerx.io) with the heroku app url
+```
+Webhook Url: <YOUR-HEROKU-APP-URL>/machaao/hook
+```
+
+### Test your bot:
+Visit: ```https://messengerx.io/<your-character-name>```
+
+## Notes / Additional Resources ##
+* Please note that this document isn't meant to be used as a guide for production environment setup.
+* Reach out to us on [Twitter](https://twitter.com/messengerxio) for any queries
---
+++ logic/bot_logic.py
@@ -0,0 +1,247 @@
+import base64
+import json
+import os
+from datetime import datetime
+
+import requests
+from dotenv import load_dotenv
+from langchain import HuggingFaceHub, LLMChain
+from langchain.prompts import PromptTemplate
+from requests.structures import CaseInsensitiveDict
+from transformers import AutoTokenizer
+
+ban_words = ["nigger", "negro", "nazi", "faggot", "murder", "suicide"]
+
+# list of banned input words
+c = 'UTF-8'
+
+
+def send(url, headers, payload=None):
+    if payload:
+        print("sending post to platform: " + str(payload))
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        print("response from the platform: " + str(response.text))
+    else:
+        response = requests.request("GET", url, headers=headers)
+
+    return response
+
+
+# don't change for sanity purposes
+def get_details(api_token, base_url):
+    _cache_ts_param = str(datetime.now().timestamp())
+    e = "L3YxL2JvdHMvY29uZmlnP3Y9"
+    check = base64.b64decode(e).decode(c)
+    url = f"{base_url}{check}{_cache_ts_param}"
+    headers = {
+        "api_token": api_token,
+        "Content-Type": "application/json",
+    }
+
+    response = send(url, headers)
+
+    if response and response.status_code == 200:
+        return response.json()
+    else:
+        return {}
+
+
+class BotLogic:
+    def __init__(self, server_session_create_time):
+        # Initializing Config Variables
+        load_dotenv()
+
+        self.api_token = os.environ.get("API_TOKEN")
+        self.base_url = os.environ.get("BASE_URL", "https://ganglia.machaao.com")
+        self.name = os.environ.get("NAME")
+        self.limit = os.environ.get("LIMIT", 'True')
+        self.server_session_create_time = server_session_create_time
+        self.model = os.environ.get("MODEL_NAME")
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model)
+
+        # Bot config
+        self.top_p = os.environ.get("TOP_P", 1.0)
+        self.top_k = os.environ.get("TOP_K", 20)
+        self.temp = os.environ.get("TEMPERATURE", 0.3)
+        self.max_length = os.environ.get("MAX_LENGTH", 50)
+        self.validate_bot_params()
+
+    # noinspection DuplicatedCode
+    def validate_bot_params(self):
+        print("Setting up Bot server with parameters:")
+        if self.top_p is not None and self.temp is not None:
+            print("Temperature and Top_p parameters can't be used together. Using default value of top_p")
+            self.top_p = 0.5
+
+        if self.temp is not None:
+            self.temp = float(self.temp)
+            if self.temp < 0.0 or self.temp > 1.0:
+                raise Exception("Temperature parameter must be between 0 and 1")
+        else:
+            self.temp = 0.8
+        print(f"Temperature = {self.temp}")
+
+        if self.top_p is not None:
+            self.top_p = float(self.top_p)
+            if self.top_p <= 0.0 or self.top_p >= 1.0:
+                raise Exception("Top_p parameter must be between 0 and 1")
+        else:
+            self.top_p = 1.0
+        print(f"Top_p = {self.top_p}")
+
+        if self.top_k is not None:
+            self.top_k = int(self.top_k)
+            if self.top_k > 1000:
+                raise Exception("Top_k parameter must be less than 1000")
+        else:
+            self.top_k = 50
+        print(f"Top_k = {self.top_k}")
+
+        if self.max_length is not None:
+            self.max_length = int(self.max_length)
+            if self.max_length > 1024:
+                raise Exception("Max_length parameter must be less than 1024")
+        else:
+            self.max_length = 50
+        print(f"Max_Length = {self.max_length}")
+
+    @staticmethod
+    def read_prompt(name):
+        file_name = "./logic/prompt.txt"
+        with open(file_name) as f:
+            prompt = f.read()
+
+        return prompt.replace("[name]", f"{name}")
+
+    def get_recent(self, user_id: str, current_session=True):
+        limit = 5
+        url = f"{self.base_url}/v1/conversations/history/{user_id}/{limit}"
+
+        headers = CaseInsensitiveDict()
+        headers["api_token"] = self.api_token
+        headers["Content-Type"] = "application/json"
+
+        resp = requests.get(url, headers=headers, timeout=10)
+
+        if resp.status_code == 200:
+            messages = resp.json()
+            if current_session:
+                filtered_messages = list()
+                for message in messages:
+                    create_time_stamp = message.get("_created_at")
+                    create_time = datetime.strptime(create_time_stamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+                    if create_time > self.server_session_create_time:
+                        filtered_messages.append(message)
+
+                while len(filtered_messages) > 0 and (filtered_messages[0].get("type") == "outgoing"):
+                    _ = filtered_messages.pop(0)
+
+                return filtered_messages
+            else:
+                return messages
+
+    @staticmethod
+    def parse(data):
+        msg_type = data.get('type')
+        if msg_type == "outgoing":
+            msg_data = json.loads(data['message'])
+            msg_data_2 = json.loads(msg_data['message']['data']['message'])
+
+            if msg_data_2 and msg_data_2.get('text', ''):
+                text_data = msg_data_2['text']
+            elif msg_data_2 and msg_data_2['attachment'] and msg_data_2['attachment'].get('payload', '') and \
+                    msg_data_2['attachment']['payload'].get('text', ''):
+                text_data = msg_data_2['attachment']['payload']['text']
+            else:
+                text_data = ""
+        else:
+            msg_data = json.loads(data['incoming'])
+            if msg_data['message_data']['text']:
+                text_data = msg_data['message_data']['text']
+            else:
+                text_data = ""
+
+        return msg_type, text_data
+
+    def core(self, req: str, label: str, user_id: str, client: str, sdk: str, action_type: str, api_token: str):
+        print(
+            "input text: " + req + ", label: " + label + ", user_id: " + user_id + ", client: " + client + ", sdk: " + sdk
+            + ", action_type: " + action_type + ", api_token: " + api_token)
+
+        bot = get_details(api_token, self.base_url)
+        name = self.name
+
+        if not bot:
+            return False, "Oops, the chat bot doesn't exist or is not active at the moment"
+        else:
+            name = bot.get("displayName", name)
+
+        valid = True
+
+        recent_text_data = self.get_recent(user_id)
+        recent_convo_length = len(recent_text_data)
+
+        print(f"len of history: {recent_convo_length}")
+
+        banned = any(ele in req for ele in ban_words)
+
+        messages = [
+            {"role": "user", "content": "Hello, how are you?"},
+            {"role": f"assistant", "content": "I'm doing great. How can I help you today?"}
+        ]
+
+        if banned:
+            print(f"banned input:" + str(req) + ", id: " + user_id)
+            return False, "Oops, please refrain from such words"
+
+        for text in recent_text_data[::-1]:
+            msg_type, text_data = self.parse(text)
+
+            if text_data:
+                e_message = "Oops," in text_data and "connect@machaao.com" in text_data
+
+                if msg_type is not None and not e_message:
+                    # outgoing msg - bot msg
+                    messages.append({
+                        "role": f"assistant",
+                        "content": text_data
+                    })
+                else:
+                    # incoming msg - user msg
+                    messages.append({
+                        "role": "user",
+                        "content": text_data
+                    })
+
+        try:
+            reply = self.process_via_huggingface(name, messages)
+            return valid, reply
+        except Exception as e:
+            print(f"error - {e}, for {user_id}")
+            return False, "Oops, I am feeling a little overwhelmed with messages\nPlease message me later"
+
+    def process_via_huggingface(self, name, messages):
+        parameters = {
+            "max_new_tokens": self.max_length,
+            "temperature": self.temp,
+            "top_k": self.top_k,
+            "top_p": self.top_p
+        }
+
+        print(f"processing for {self.model}")
+
+        hub_llm = HuggingFaceHub(repo_id=self.model, model_kwargs=parameters)
+
+
+        templ = self.tokenizer.apply_chat_template(messages, tokenize=False)
+
+        _prompt = self.read_prompt(name)
+
+        prompt = PromptTemplate(
+            input_variables=["conversation"],
+            template=_prompt + ": {conversation}"
+        )
+
+        hub_chain = LLMChain(prompt=prompt, llm=hub_llm, verbose=True)
+        resp = hub_chain.run(templ)
+        return resp
